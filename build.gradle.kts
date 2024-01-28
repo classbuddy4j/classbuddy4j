9eccb3a1979e90f412c81a94718468e2848f5507
sullis <github@seansullivan.com>
2024-01-27 18:05:51 -0800

    assertj 3.25.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     implementation("net.bytebuddy:byte-buddy-agent:$byteBuddyVersion")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.assertj:assertj-core:3.25.2")
 }
 
 tasks.test {
