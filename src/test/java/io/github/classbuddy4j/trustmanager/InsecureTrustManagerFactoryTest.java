2117e7f4981f040ecd25ef4e0e408f47a428bd8c
sullis <github@seansullivan.com>
2024-02-15 21:34:33 -0800

    enhance InsecureTrustManagerFactoryTest

--- src/test/java/io/github/classbuddy4j/trustmanager/InsecureTrustManagerFactoryTest.java
+++ src/test/java/io/github/classbuddy4j/trustmanager/InsecureTrustManagerFactoryTest.java
@@ -6,10 +6,12 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 
 import static io.github.classbuddy4j.trustmanager.InsecureTrustManagerFactory.DEFAULT_ALGORITHM;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 
@@ -30,5 +32,17 @@ public class InsecureTrustManagerFactoryTest {
         X509Certificate[] chain = new X509Certificate[0];
         trustManager.checkClientTrusted(chain, null);
         trustManager.checkServerTrusted(chain, null);
+    }
+
+    @Test
+    void exceptionThrownWhenAlgorithmIsBogus() {
+        assertThatThrownBy(() -> { TrustManagerFactory.getInstance("bogus"); })
+                .isInstanceOf(NoSuchAlgorithmException.class)
+                .hasMessage("bogus TrustManagerFactory not available");
+    }
+
+    @Test
+    void defaultAlgorithmIsPkix() {
+        assertThat(TrustManagerFactory.getDefaultAlgorithm()).isEqualTo("PKIX");
     }
 }
