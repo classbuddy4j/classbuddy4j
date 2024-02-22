d7edcf7ba9570186fe64cf49828e8b01301fc21b
sullis <github@seansullivan.com>
2024-02-22 11:24:14 -0800

    version 0.0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ apply(from="gradle/shipkit.gradle.kts")
 
 group = "io.github.classbuddy4j"
 description = "classbuddy4j"
-version = "0.0.6"
+version = "0.0.7"
 
 repositories {
     mavenCentral()
