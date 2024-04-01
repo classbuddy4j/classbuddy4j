316eb5ec311162ed589e90be254c459ffc95ae34
sullis <github@seansullivan.com>
2024-03-31 20:32:59 -0700

    bytebuddy 1.14.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
     mavenCentral()
 }
 
-val byteBuddyVersion = "1.14.12"
+val byteBuddyVersion = "1.14.13"
 
 dependencies {
     implementation("net.bytebuddy:byte-buddy:$byteBuddyVersion")
