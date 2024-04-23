636e3176cc01605f52693426bc119345d9677935
sullis <github@seansullivan.com>
2024-04-23 12:14:20 -0700

    junit 5.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ val byteBuddyVersion = "1.14.13"
 dependencies {
     implementation("net.bytebuddy:byte-buddy:$byteBuddyVersion")
     implementation("net.bytebuddy:byte-buddy-agent:$byteBuddyVersion")
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.2")
 }
